d1efeed1fd6480e2f01ed4127df43c6193efa8cf
Gavin Corkery <GavinCorkery@hotmail.com>
2022-01-12 10:01:11 -0500

    Created and tested user endpoints
    As of now there is no token authorization. This will be added in a future sprint.

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 const mongoose = require('./db/mongoose')
 
 //Load in models
 const { Timer } = require('./db/models/timer.model');
+const { User } = require('./db/models/user.model')
 
 //Load middleware
 app.use(bodyParser.json());
+
+//Connection confirmation
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+})
+
+//Timer model endpoints
 
 /* 
  * GET /timers
@@ -60,6 +67,44 @@ app.delete('/timers/:id', (req, res) => {
     // Delete specified list
 })
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+//User model endpoints
+
+/* 
+ * GET /users
+ * Purpose: Get user document based on id passed in JSON request body
+*/
+app.get('/users', (req, res) => {
+    // Return user object if match is found
+    userId = req.body.id
+    User.findById(userId).then((users) => {
+        res.send(users)
+    }).catch((err) => {
+        res.send("There is no matching users or there was an error in your request.")
+        res.send(err)
+    })
+})
+
+/* 
+ * POST /users
+ * Purpose: Create a new user
+*/
+app.post('/users', (req, res) => {
+    // Make a new user and return new user document back, including ID
+    // User information fields will be passed in via JSON request body
+    let firstName = req.body.firstName;
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let newUser = new User({
+        firstName,
+        username,
+        email,
+        password
+    })
+
+    newUser.save().then((userDoc) => {
+        //Full timer document is returned
+        res.send(userDoc);
+    })
 })
